4189487f0c3161cde99a4dae628dd63ad0d8bb58
Chee-Chan Keng <kengcc@gmail.com>
2025-12-27 12:37:07 +0800

    exercise 2.9: add filter to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,13 @@
 import {useState} from "react"
 import Person from "./components/Person"
+import {use} from "react"
 
 const App = (props) => {
   // const [persons, setPersons] = useState([{name: "Arto Hellas"}])
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
-  const [showAll, setShowAll] = useState(true)
+  const [filter, setFilter] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -40,14 +41,23 @@ const App = (props) => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-  const personsToShow = showAll
-    ? persons
-    : persons.filter((person) => person.important === true)
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <div>
+        filter shown with:{" "}
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
