25992a69addda33aa3870f01421aefdd223ef66b
Chee-Chan Keng <kengcc@gmail.com>
2026-01-02 15:14:31 +0800

    uibuild

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -65,6 +65,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001 // default 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
