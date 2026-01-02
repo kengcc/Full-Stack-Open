0bbea052ad49fc099efc237896e1c0d6d5b9cbf4
Chee-Chan Keng <kengcc@gmail.com>
2026-01-02 22:37:00 +0800

    exercise 3.15-3.18 - updated phonebook to use error handler middleware
    modified:   part3/phonebook-backend/index.js

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,14 +1,10 @@
 require("dotenv").config()
 
 const Person = require("./models/person")
-const mongoose = require("mongoose")
 
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
-app.use(express.static("dist")) // show frontend build files
-
-app.use(express.json())
 
 morgan.token("body", (req) => {
   if (req.method !== "POST") {
@@ -18,26 +14,34 @@ morgan.token("body", (req) => {
   return JSON.stringify(req.body)
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({error: "malformatted id"})
+  }
+
+  next(error)
+}
+
+app.use(express.static("dist")) // show frontend build files
+app.use(express.json())
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 )
-
-let persons = []
 
 app.get("/", (request, response) => {
   response.send("<h1>Welcome to phonebook backend!!!</h1>")
 })
 
-app.get("/info", async (request, response) => {
-  try {
-    const count = await Person.countDocuments({})
-    const info = `Phonebook has info for ${count} people`
-    const timestamp = new Date()
-    response.send(`<p>${info}</p><p>${timestamp}</p>`)
-  } catch (error) {
-    console.error("failed to fetch person count:", error.message)
-    response.status(500).send("unable to retrieve phonebook info")
-  }
+app.get("/info", (request, response, next) => {
+  Person.countDocuments({})
+    .then((count) => {
+      const info = `Phonebook has info for ${count} people`
+      const timestamp = new Date()
+      response.send(`<p>${info}</p><p>${timestamp}</p>`)
+    })
+    .catch((error) => next(error))
 })
 
 app.get("/api/persons", (request, response) => {
@@ -46,17 +50,24 @@ app.get("/api/persons", (request, response) => {
   })
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person)
-  })
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).send({error: "person not found"})
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id
-  persons = persons.filter((person) => person.id !== id)
-
-  response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
@@ -94,6 +105,33 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const {name, number} = request.body
+
+  if (!name || !number) {
+    return response.status(400).json({
+      error: "name and number are required",
+    })
+  }
+
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
