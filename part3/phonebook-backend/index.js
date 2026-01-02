ae71d7873b0704613ee4f6dc6a0516fe6d0929d1
Chee-Chan Keng <kengcc@gmail.com>
2026-01-02 11:33:36 +0800

    phonebook_ui_build

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
+app.use(express.static("dist")) // show frontend build files
 
 app.use(express.json())
 
-const cors = require("cors")
-app.use(cors())
+//const cors = require("cors")
+//app.use(cors())
 
 /*
 const requestLogger = (request, response, next) => {
@@ -132,6 +133,6 @@ app.post("/api/persons", (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
