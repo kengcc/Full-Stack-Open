8d2dbd462c1475e665bad529018745ff83f61f75
Chee-Chan Keng <kengcc@gmail.com>
2026-01-03 21:36:44 +0800

    added eslint config and fix coding style to eslint standards
    	new file:   part3/notes-backend/eslint.config.mjs
    	modified:   part3/notes-backend/index.js
    	modified:   part3/notes-backend/models/note.js
    	modified:   part3/notes-backend/mongo.js
    	modified:   part3/notes-backend/package-lock.json
    	modified:   part3/notes-backend/package.json

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,70 +1,70 @@
-require("dotenv").config()
+require('dotenv').config()
 
-const Note = require("./models/note")
-const express = require("express")
+const Note = require('./models/note')
+const express = require('express')
 
 const app = express()
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method)
-  console.log("Path:  ", request.path)
-  console.log("Body:  ", request.body)
-  console.log("---")
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
   next()
 }
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({error: "malformatted id"})
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({error: error.message})
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
 }
 
-app.use(express.static("dist")) // show frontend build files
+app.use(express.static('dist')) // show frontend build files
 app.use(express.json())
 app.use(requestLogger)
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!!!</h1>")
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!!!</h1>')
 })
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes)
   })
 })
 
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
         response.json(note)
       } else {
-        response.status(404).send({error: "note not found"})
+        response.status(404).send({ error: 'note not found' })
       }
     })
     .catch((error) => next(error))
 })
 
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end()
     })
     .catch((error) => next(error))
 })
 
-app.post("/api/notes", (request, response, next) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
   console.log(body)
 
   if (!body.content) {
-    return response.status(400).json({error: "content missing"})
+    return response.status(400).json({ error: 'content missing' })
   }
 
   const note = new Note({
@@ -80,8 +80,8 @@ app.post("/api/notes", (request, response, next) => {
     .catch((error) => next(error))
 })
 
-app.put("/api/notes/:id", (request, response, next) => {
-  const {content, important} = request.body
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
   Note.findById(request.params.id)
     .then((note) => {
@@ -100,7 +100,7 @@ app.put("/api/notes/:id", (request, response, next) => {
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: "unknown endpoint"})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
--- part3/notes-backend/mongo.js
+++ part3/notes-backend/mongo.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument")
+  console.log('give password as argument')
   process.exit(1)
 }
 
@@ -9,16 +9,16 @@ const password = process.argv[2]
 
 const url = `mongodb+srv://kengcc_db_user:${password}@cluster0.bqvvjb5.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url, {family: 4})
+mongoose.connect(url, { family: 4 })
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
-const Note = mongoose.model("Note", noteSchema)
+const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
 //   content: "HTML is easy 4",
