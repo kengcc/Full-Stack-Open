d94532af795e8878201365b345b4764fbab99944
Chee-Chan Keng <kengcc@gmail.com>
2026-01-10 16:09:52 +0800

    created part 5

---
+++ part5/notes-backend/controllers/login.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash)
+
+  // !! course provided passwordHash for user mluukkai is obsolete,
+  // have to manually update user's passowrdHash field in database first!!
+  // user console.log below to get new hash, then update mongodb table
+  // console.log('password', password)
+  // console.log('bcrypt password', await bcrypt.hash(password, 10))
+  // console.log('passwordHash', user.passwordHash)
+  // console.log('passwordCorrect', passwordCorrect)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password',
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  // token expires in 60*60 seconds, that is, in one hour
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 60 * 60,
+  })
+
+  response.status(200).send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
