ebce52002f4931ceef5592ece7c381ba29c1cbc0
Chee-Chan Keng <kengcc@gmail.com>
2026-01-10 21:51:22 +0800

    5.2: localStorage & logout implementation

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,13 +15,33 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      if (user) {
+        setUser(user)
+        blogService.setToken(user.token)
+      }
+    }
+  }, [])
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
+
     try {
       const user = await loginService.login({
         username,
         password,
       })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -69,7 +89,12 @@ const App = () => {
       {!user && loginForm()}
       {user && (
         <div>
-          <p>{user.name} logged in</p>
+          <p>
+            {user.name} logged in{' '}
+            <button type='button' onClick={handleLogout}>
+              logout
+            </button>
+          </p>
           {blogList()}
         </div>
       )}
