77c15c942817e091a7d2c9d4da9e8d853dc4725a
Chee-Chan Keng <kengcc@gmail.com>
2026-01-10 15:26:00 +0800

    exercise 4.20: refactor tokenExtractor to middleware

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,15 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {
