9f08dc8389dfc3992041016bd920f3a108488256
Chee-Chan Keng <kengcc@gmail.com>
2026-05-26 11:57:52 +0800

    Refactor tests in note_app.spec.js to use beforeEach for setup

--- notes-e2e/tests/note_app.spec.js
+++ notes-e2e/tests/note_app.spec.js
@@ -1,9 +1,11 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
+  })
 
+  test('front page can be opened', async ({ page }) => {
     const locator = page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(
@@ -14,13 +16,10 @@ describe('Note app', () => {
   })
 
   test('user can log in', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-
     await page.getByRole('button', { name: 'login' }).click()
     await page.getByLabel('username').fill('mluukkai')
     await page.getByLabel('password').fill('salainen')
     await page.getByRole('button', { name: 'login' }).click()
-
     await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
   })
 })
