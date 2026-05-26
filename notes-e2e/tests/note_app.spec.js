b1906a1317446e6c571d0153e679273cb838fa77
Chee-Chan Keng <kengcc@gmail.com>
2026-05-26 11:56:27 +0800

    Add Playwright configuration and initial tests for notes app

---
+++ notes-e2e/tests/note_app.spec.js
@@ -0,0 +1,26 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Note app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    const locator = page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(
+      page.getByText(
+        'Note app, Department of Computer Science, University of Helsinki 2025',
+      ),
+    ).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('mluukkai')
+    await page.getByLabel('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
+})
